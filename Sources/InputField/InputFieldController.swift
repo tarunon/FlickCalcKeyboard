b5ece3aefe0d9169a00f0cc685dfcb8a72b83054
tarunon <croissant9603@gmail.com>
2021-09-23 13:55:14 +0900

    modify: fix inputview truncate

--- Sources/InputField/InputFieldController.swift
+++ Sources/InputField/InputFieldController.swift
@@ -10,6 +10,21 @@ import SwiftUI
 import UIKit
 
 @MainActor
+class InputTextField: UITextField {
+  override func textRect(forBounds bounds: CGRect) -> CGRect {
+    return super.textRect(forBounds: bounds).inset(
+      by: .init(top: 0.0, left: 0.0, bottom: 0.0, right: 50.0)
+    )
+  }
+
+  override func editingRect(forBounds bounds: CGRect) -> CGRect {
+    return super.editingRect(forBounds: bounds).inset(
+      by: .init(top: 0.0, left: 0.0, bottom: 0.0, right: 50.0)
+    )
+  }
+}
+
+@MainActor
 @objc protocol InputFieldControllerDelegate: NSObjectProtocol {
   @objc optional func inputFieldDidChangeSelection(_ controller: InputFieldController)
 }
@@ -17,9 +32,10 @@ import UIKit
 @MainActor
 class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITextFieldDelegate {
   lazy var textField: UITextField = build {
-    let textField = UITextField()
+    let textField = InputTextField()
+    textField.translatesAutoresizingMaskIntoConstraints = false
     textField.delegate = self
-    textField.borderStyle = .roundedRect
+    textField.borderStyle = .none
     textField.text = ""
     textField.selectedTextRange = textField.textRange(
       from: textField.beginningOfDocument,
@@ -29,11 +45,27 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
     return textField
   }
 
-  lazy var textMarker = UIHostingController(
-    rootView: TextMarker(
-      flushing: true
+  lazy var scrollView: UIScrollView = build {
+    let scrollView = UIScrollView()
+    scrollView.backgroundColor = UIColor.tertiarySystemBackground
+    scrollView.layer.cornerRadius = 5.0
+    scrollView.panGestureRecognizer.minimumNumberOfTouches = 3
+    scrollView.showsHorizontalScrollIndicator = false
+    scrollView.showsVerticalScrollIndicator = false
+    return scrollView
+  }
+
+  lazy var textMarker: UIHostingController<TextMarker> = build {
+    let textMarker = UIHostingController(
+      rootView: TextMarker(
+        flushing: true
+      )
     )
-  )
+    textMarker.view.frame = .init(x: 7.0, y: 3.0, width: 2.0, height: 22.0)
+    textMarker.view.backgroundColor = .clear
+    return textMarker
+  }
+
   lazy var dragGestureRecognizer: UILongPressGestureRecognizer = build {
     let dragGestureRecognizer = UILongPressGestureRecognizer(
       target: self,
@@ -44,6 +76,7 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
     return dragGestureRecognizer
   }
   var delegate: InputFieldControllerDelegate?
+  var shouldUpdateCursor = false
 
   var errorMessage: String = "" {
     didSet {
@@ -58,11 +91,29 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
   }
 
   override func loadView() {
-    view = textField
+    view = scrollView
+    scrollView.addSubview(textField)
+    NSLayoutConstraint.activate([
+      textField.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      textField.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 5.0),
+      textField.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: 5.0),
+      textField.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      textField.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+      textField.widthAnchor.constraint(
+        greaterThanOrEqualTo: scrollView.widthAnchor,
+        constant: -10.0
+      ),
+    ])
     addChild(textMarker)
-    view.addSubview(textMarker.view)
-    textMarker.view.frame = .init(x: 7.0, y: 3.0, width: 2.0, height: 22.0)
+    scrollView.addSubview(textMarker.view)
     textMarker.didMove(toParent: self)
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    if shouldUpdateCursor {
+      updateCursor()
+    }
   }
 
   var cursor: NSRange? {
@@ -97,6 +148,14 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
   }
 
   func updateCursor() {
+    // Note: Should wait textField.textInputView rendering before update curosr
+    if textField.textInputView.frame.width
+      < textField.attributedText?.boundingRect(with: .zero, options: [], context: nil).width ?? 0.0
+    {
+      view.setNeedsLayout()
+      return
+    }
+    shouldUpdateCursor = false
     if let start = textField.selectedTextRange?.start,
       let end = textField.selectedTextRange?.end
     {
@@ -105,13 +164,20 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
         textField.textInputView.frame,
         to: textField
       ).origin
-      textMarker.view.frame = rect.applying(
-        .identity.translatedBy(x: inputOrigin.x / 2, y: inputOrigin.y / 2)
+      textMarker.view.frame = scrollView.convert(
+        rect.applying(
+          .identity.translatedBy(x: inputOrigin.x / 2, y: inputOrigin.y / 2)
+        ),
+        from: textField
       )
       // Notes: √ change line height.
       textMarker.view.frame.origin.y = 3.0
       textMarker.view.frame.size.height = 22.0
       textMarker.rootView = .init(flushing: start == end)
+      scrollView.scrollRectToVisible(
+        textMarker.view.frame.inset(by: .init(top: 0.0, left: 10.0, bottom: 0.0, right: 10.0)),
+        animated: false
+      )
     }
   }
 
@@ -126,7 +192,6 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
     if start == end {
       textField.selectedTextRange = textField.textRange(from: cursor, to: cursor)
     }
-    updateCursor()
   }
 
   func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
@@ -134,7 +199,8 @@ class InputFieldController: UIViewController, UIGestureRecognizerDelegate, UITex
   }
 
   func textFieldDidChangeSelection(_ textField: UITextField) {
-    updateCursor()
+    shouldUpdateCursor = true
+    view.setNeedsLayout()
     delegate?.inputFieldDidChangeSelection?(self)
   }
 
